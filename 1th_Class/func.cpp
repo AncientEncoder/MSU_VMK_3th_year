e39ef7ccc896ab46b28539f9abeede8ec513781d
王谢雨 <varianovsky@outlook.com>
2021-09-02 22:39:17 +0800

    Final test vscode

---
+++ 1th_Class/func.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int func(void){
+    cout<<"this is func!"<<endl;
+    return 0;
+}
--- 1th_Class/main.cpp
+++ 1th_Class/main.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
+#include "func.cpp"
 using namespace std;
 int main(int argc,char* argv[]){
-    cout<<"envo test";
-    cout<<"test push";
+    cout<<"envo test"<<endl;
+    cout<<"test push"<<endl;
+    int a=0;
+    cin>>a;
+    int c= func();
+    cout<<"second";
     return 0;
 }
